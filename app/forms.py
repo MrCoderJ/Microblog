1bda2e3bd96f07ca9e6937ce3f056d5e28e9fc83
elisha <ebehelisha@gmail.com>
2020-01-27 12:34:52 +0000

    login details

--- app/forms.py
+++ app/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BoooleanField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
-    rember_me = BoooleanField('Remember Me')
+    remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign In')
